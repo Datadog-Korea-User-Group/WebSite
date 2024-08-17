1e89c7b99d91ae265e04bdbb2deb5ecdb7e115c6
Changhyeon Yoon <yoon@changhyeon.net>
2024-08-17 19:21:20 +0900

    Fix : Sitemap url

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,31 +3,31 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://datadog-korea-user-group.github.io/",
+      url: "https://datadogkrug.vercel.app/",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: "https://datadog-korea-user-group.github.io/about",
+      url: "https://datadogkrug.vercel.app/about",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://datadog-korea-user-group.github.io/events",
+      url: "https://datadogkrug.vercel.app/events",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://datadog-korea-user-group.github.io/events/1",
+      url: "https://datadogkrug.vercel.app/events/1",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://datadog-korea-user-group.github.io/blog",
+      url: "https://datadogkrug.vercel.app/blog",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
