ce51b9a6c02d9c15d5a7fc572e45b240146a76f1
Changhyeon Yoon <yoon@changhyeon.net>
2024-08-17 20:22:40 +0900

    Fix : ENV

--- src/components/Datadog.tsx
+++ src/components/Datadog.tsx
@@ -6,12 +6,12 @@ import { datadogRum } from "@datadog/browser-rum";
 
 export const Datadog = ({ children }: { children: React.ReactNode }) => {
   React.useEffect(() => {
-    if (process.env.NODE_ENV === "development") return;
-    if (!process.env.DATADOG_APPLICATION_ID || !process.env.DATADOG_CLIENT_TOKEN) return;
-
+    // if (process.env.NODE_ENV === "development") return;
+    console.log(process.env.NEXT_PUBLIC_DATADOG_APPLICATION_ID, process.env.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN);
+    if (!process.env.NEXT_PUBLIC_DATADOG_APPLICATION_ID || !process.env.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN) return;
     datadogRum.init({
-      applicationId: process.env.DATADOG_APPLICATION_ID,
-      clientToken: process.env.DATADOG_CLIENT_TOKEN,
+      applicationId: process.env.NEXT_PUBLIC_DATADOG_APPLICATION_ID,
+      clientToken: process.env.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN,
       site: "datadoghq.com",
       service: "datadogkrug",
       sessionSampleRate: 100,
@@ -21,6 +21,7 @@ export const Datadog = ({ children }: { children: React.ReactNode }) => {
       trackLongTasks: true,
       defaultPrivacyLevel: "allow",
     });
-  });
+    console.log("Datadog initialized");
+  }, []);
   return <>{children}</>;
 };
