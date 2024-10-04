0fc292068f23997795cb0ff249e525e3e6cc4a55
Changhyeon Yoon <yoon@changhyeon.net>
2024-10-05 07:38:02 +0900

    Fix : Text

--- src/components/Event.tsx
+++ src/components/Event.tsx
@@ -94,7 +94,7 @@ export const SingleEvent = ({ event }: { event: Event }) => {
         target='__blank'
         className='fixed bottom-10 md:right-10 max-md:bottom-4 max-md:left-5 inline-flex items-center gap-1 group/button rounded-full hover:scale-105 focus:outline-none transition ring-offset-gray-900 bg-gray-800 text-white shadow-lg shadow-black/20 sm:backdrop-blur-sm group-hover/button:bg-gray-50/15 group-hover/button:scale-105 focus-visible:ring-1 focus-visible:ring-offset-2 ring-gray-50/60 text-sm font-medium px-4 py-2 mt-auto origin-left'
       >
-        Go to Event Detail
+        Go to MeetUp
         <svg
           xmlns='http://www.w3.org/2000/svg'
           width='24'
